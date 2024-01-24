720eb24e15fabd5d9c57ecb5db6f3d4661e22b3d
ivancho894 <serralta.ivan@gmail.com>
2024-01-24 13:11:35 -0300

    entire mod

---
+++ src/content_option.js
@@ -0,0 +1,158 @@
+const logotext = "SERRADEV";
+const meta = {
+    title: "Ivan Serralta",
+    description: "I’m Ivan Serralta Systems of Information Engineer student, Full stack devloper and Agile coach",
+};
+
+const introdata = {
+    title: "I’m Ivan Serralta",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Passionate about creating innovative web solutions with an eye for design. Open to collaborations and new opportunities. Let's connect!",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Results-driven Systems of Information student and adept full-stack web developer with a passion for creating innovative digital solutions. Proficient at amalgamating technology and information systems to craft robust web applications.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Developer Project",
+        where: "E-commerce",
+        date: "2023",
+    },
+    {
+        jobtitle: "Front desk tasks",
+        where: "Park City Mountain, Vail resort",
+        date: "2022",
+    },
+    {
+        jobtitle: "It intern",
+        where: "Regional Municipality",
+        date: "2018",
+    },
+];
+
+const skills = [
+    {
+        name: "English (C2 Proficient)",
+        value: 80
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "HTML",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 60,
+    },    
+    {
+        name: "Node.js",
+        value: 60,
+    },
+    {
+        name: "github",
+        value: 75,
+    },
+    {
+        name: "Trello",
+        value: 70
+    },
+
+    {
+        name: "Microsoft Office Apps",
+        value: 90
+    },
+
+    {
+        name: "Srum",
+        value: 85
+    },
+
+    {
+        name: "Canva",
+        value: 70
+    },
+
+    {
+        name: "SO support (Linux, Windows, MacOs)",
+        value: 70
+    },
+
+    
+];
+
+const services = [
+    {
+        title: "Web Development",
+        description: "Passionate about crating innovate web solutions with an eye for design."
+    },{
+        title: "Agile coach",
+        description: "Scrum and Agile frameworks,fostering effective project managment.",
+    },
+
+
+];
+
+const dataportfolio = [{
+        img: "https://www.beejaacademy.com/images/course/full-stack.webp",
+        description: "Full Stack Developer.",
+        link: "http://d2rf2c6bvm78n9.cloudfront.net/new-cert?id=319584dfec91e05b2e18b62626d9f2aeac790e8bac4350f89d528ca7c727dced",
+    },
+    {
+        img: "https://a.storyblok.com/f/79503/250x250/ddb3f17e97/group-3.png",
+        description: "English C2",
+        link: "https://www.efset.org/cert/rtNV2N",
+    },    
+    {
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+        description: "JavaScript Algorithms and Data Structures",
+        link: "https://www.freecodecamp.org/certification/fcc9ee02363-175a-457c-9679-2a27bc33fec9/javascript-algorithms-and-data-structures",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ivanserradev@gmail.com",
+    YOUR_FONE: "+54 3541-566295xº",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ivancho894",
+    instagram: "https://www.instagram.com/ivanserralta/",
+    linkedin: "https://www.linkedin.com/in/ivan-serralta/",
+    twitter: false,
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
